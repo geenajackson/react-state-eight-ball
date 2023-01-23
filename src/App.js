11e68fe36da25c04a83211e72cf39604903c4acc
Geena Jackson <geena.jackson96@gmail.com>
2023-01-23 10:12:43 -0600

    display EightBall component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import EightBall from './EightBall';
 
 function App() {
   return (
     <div className="App">
       <h1>Magic Eight Ball</h1>
+      <EightBall />
     </div>
   );
 }
