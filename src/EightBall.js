9baa880f96ae39451ff8d17c2487aab76b4d30d6
Geena Jackson <geena.jackson96@gmail.com>
2023-01-23 10:12:08 -0600

    initalize EightBall component

---
+++ src/EightBall.js
@@ -0,0 +1,14 @@
+import answers from "./answers";
+import "./EightBall.css"
+
+import { useState } from "react";
+
+const EightBall = () => {
+    return (
+        <div className="EightBall">
+            <p>Think of a Question</p>
+        </div>
+    )
+};
+
+export default EightBall;
