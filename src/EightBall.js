c9201443b2aa3313cf22652045ee9ecb922a681c
Geena Jackson <geena.jackson96@gmail.com>
2023-01-23 11:02:55 -0600

    add onClick event and counter logic

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,9 +4,29 @@ import "./EightBall.css"
 import { useState } from "react";
 
 const EightBall = () => {
+    const [msg, setMsg] = useState("Think of a Question");
+    const [color, setColor] = useState("black");
+    const backgroundColor = { backgroundColor: color };
+    //ball stalls if a counter is not used; will freeze on a message and color.
+    let [counter, setCounter] = useState(0);
+
+    const setBall = () => {
+        setMsg(answer.msg);
+        setColor(answer.color);
+        setCounter(counter += 1)
+    }
+    const answer = answers[(Math.floor(Math.random() * answers.length))]
+
     return (
         <div className="EightBall">
-            <p>Think of a Question</p>
+            <div className="EightBall-Ball" style={backgroundColor} onClick={setBall}>
+                <p>
+                    {msg}
+                </p>
+            </div>
+            <p>
+                You've decided your fate <b>{counter}</b> time{counter !== 1 ? "s" : ""}.
+            </p>
         </div>
     )
 };
